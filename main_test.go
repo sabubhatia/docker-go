4781c5c8f48a7e9583a6b55a5f0e42a5d4108094
Sarabjit Bhatia <sarabjitbhatia@Sarabjits-MacBook-Pro.local>
2023-04-08 18:37:14 +0800

    added test7

--- main_test.go
+++ main_test.go
@@ -89,3 +89,17 @@ func TestMain6(t *testing.T) {
 		t.Errorf("status code expected %d got %d", http.StatusOK, resp.Result().StatusCode)
 	}
 }
+
+func TestMain7(t *testing.T) {
+	resp := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "localhost:8080/", nil)
+	frontend(resp, req)
+
+	if resp.Header().Get("content-type") != "text/html" {
+		t.Errorf("content-type expected %s got %s", "text/html; charset=utf-8", resp.Header().Get("content-type"))
+	}
+
+	if resp.Result().StatusCode != http.StatusOK {
+		t.Errorf("status code expected %d got %d", http.StatusOK, resp.Result().StatusCode)
+	}
+}
